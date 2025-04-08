bc6621a54bea0f53fdab340643770ea0562eca0c
Viswas <s.viswas111@gmail.com>
2025-04-08 17:06:50 -0500

    Changes

--- app.js
+++ app.js
@@ -4,14 +4,37 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Add mongoose and dotenv
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var HologramsRouter = require('./routes/Holograms');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
 
+// Import your model (assuming it's Hologram)
+const Hologram = require('./models/hologram');
 
 var app = express();
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_CON, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', async function () {
+  console.log('Connection to DB succeeded');
+  // Temporary seeding code
+  await Hologram.deleteMany({});
+  let hologram1 = new Hologram({ name: 'Holo1', brightness: 50, color: 'blue' });
+  let hologram2 = new Hologram({ name: 'Holo2', brightness: 75, color: 'red' });
+  let hologram3 = new Hologram({ name: 'Holo3', brightness: 30, color: 'green' });
+  await hologram1.save();
+  await hologram2.save();
+  await hologram3.save();
+  console.log('Sample holograms saved');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
