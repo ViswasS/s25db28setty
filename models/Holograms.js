5b5057f944084f5c112d80fec81fb99d118fa6ec
Viswas <s.viswas111@gmail.com>
2025-04-23 20:40:50 -0500

    modified

--- models/Holograms.js
+++ models/Holograms.js
@@ -1,7 +1,22 @@
-const mongoose = require("mongoose")
-const HologramsSchema= mongoose.Schema({
-origin: String,
-tone: String,
-clarity: Number});
-module.exports = mongoose.model("Holograms",
-HologramsSchema)
+const mongoose = require("mongoose");
+
+const HologramsSchema = new mongoose.Schema({
+  origin: {
+    type: String,
+    required: [true, "Origin is required"],
+    minlength: [3, "Origin must be at least 3 characters long"],
+  },
+  tone: {
+    type: String,
+    required: [true, "Tone is required"],
+    minlength: [2, "Tone must be at least 2 characters long"],
+  },
+  clarity: {
+    type: Number,
+    required: [true, "Clarity is required"],
+    min: [1, "Clarity must be at least 1"],
+    max: [10, "Clarity cannot be more than 10"], // Optional, adds range
+  },
+});
+
+module.exports = mongoose.model("Holograms", HologramsSchema);
--- routes/Holograms.js
+++ routes/Holograms.js
@@ -10,6 +10,15 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const Holograms_controlers= require('../controllers/Holograms');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 /* GET costumes */
 router.get('/', Holograms_controlers.Holograms_view_all_Page );
 
@@ -21,7 +30,7 @@ router.get('/Holograms/:id', Holograms_controlers.Holograms_list_detail);
 router.get('/create', Holograms_controlers.Holograms_create_Page);
 
 /* GET create update page */
-router.get('/update', Holograms_controlers.Holograms_update_Page);
+router.get('/update', secured, Holograms_controlers.Holograms_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', Holograms_controlers.Holograms_delete_Page);
